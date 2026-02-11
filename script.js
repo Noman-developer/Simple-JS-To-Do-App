b2084c66a991fa7b3370c92b12052086d07f51a4
Noman Javed <noman19304@gmail.com>
2026-02-11 18:09:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JS Code
 let addBtn = document.getElementById('addBtn');
-let taskInput = document.getElementById('taskInput');
+let taskInput = document.getElementById('taskInput'); 
 let taskList = document.getElementById('taskList');
 
 addBtn.addEventListener('click', function () {
